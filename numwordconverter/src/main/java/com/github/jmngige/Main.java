5b7742a16f8e765e548a4e114b23b7031eeeac76
jamesmn <jamesmngige@gmail.com>
2024-01-01 12:14:03 +0300

    number to words converter impl commit updates

--- numwordconverter/src/main/java/com/github/jmngige/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github.jmngige;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        NumbetoWordConverter converter = new NumbetoWordConverter();
-        System.out.println(converter.EnglishNumber(120078));
-    }
-}
